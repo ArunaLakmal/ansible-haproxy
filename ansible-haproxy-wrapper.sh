374741adeea48b2c66c17461aa6da143d3705e01
ArunaLakmal <arunarandunu@gmail.com>
2021-04-30 16:47:18 +0530

    updated wrapper

--- ansible-haproxy-wrapper.sh
+++ ansible-haproxy-wrapper.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-ansible-galaxy remove haproxy_role
-ansible-galaxy install -r requirements.yml
-ansible-playbook haproxy_playbook.yaml
+ROOT=/
+MODULE_DIR="ansible-haproxy"
+
+run_ansible() {
+  ansible-galaxy remove haproxy_role
+  ansible-galaxy install -r requirements.yml
+  ansible-playbook haproxy_playbook.yaml
+}
+
+cd "$ROOT"
+if [[ -d "$MODULE_DIR" ]]; then
+        rm -rf "$MODULE_DIR"
+else
+        git clone https://github.com/ArunaLakmal/ansible-haproxy.git  --branch "main" --single-branch
+        cd "$MODULE_DIR"
+        run_ansible
+fi
