fe6c9655df8a9beff76d7ede716fb0bab241ec78
ArunaLakmal <arunarandunu@gmail.com>
2021-05-01 09:15:14 +0530

    some ascii

--- scripts/deploy-haproxy.sh
+++ scripts/deploy-haproxy.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+cat <<"EOF"
+ /$$$$$$$$                  /$$        /$$$$$$                                    /$$       /$$          
+|__  $$__/                 | $$       /$$__  $$                                  | $$      | $$          
+   | $$  /$$$$$$   /$$$$$$$| $$$$$$$ | $$  \__/  /$$$$$$  /$$   /$$ /$$$$$$/$$$$ | $$$$$$$ | $$  /$$$$$$ 
+   | $$ /$$__  $$ /$$_____/| $$__  $$| $$       /$$__  $$| $$  | $$| $$_  $$_  $$| $$__  $$| $$ /$$__  $$
+   | $$| $$$$$$$$| $$      | $$  \ $$| $$      | $$  \__/| $$  | $$| $$ \ $$ \ $$| $$  \ $$| $$| $$$$$$$$
+   | $$| $$_____/| $$      | $$  | $$| $$    $$| $$      | $$  | $$| $$ | $$ | $$| $$  | $$| $$| $$_____/
+   | $$|  $$$$$$$|  $$$$$$$| $$  | $$|  $$$$$$/| $$      |  $$$$$$/| $$ | $$ | $$| $$$$$$$/| $$|  $$$$$$$
+   |__/ \_______/ \_______/|__/  |__/ \______/ |__/       \______/ |__/ |__/ |__/|_______/ |__/ \_______/
+                                                                                                         
+                                                                                                         
+                                                                                                         
+EOF
 echo "Deploying HAProxy....!!!"
 echo "Installing the repository"
 
@@ -9,7 +22,8 @@ sudo apt-get install ansible -y
 
 echo "Setting up unit files"
 
-cp ../units/* /etc/systemd/system/
+cp units/* /etc/systemd/system/
+cp ansible-haproxy-wrapper.sh /usr/local/bin/
 
 systemctl enable lucidwrapper.service
 systemctl enable lucidwrapper.timer
