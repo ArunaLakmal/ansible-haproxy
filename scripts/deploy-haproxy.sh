0d1c2f62d3504c1c9783aaa8f5c07b570552ced1
ArunaLakmal <arunarandunu@gmail.com>
2021-05-01 09:10:24 +0530

    automated ppa

--- scripts/deploy-haproxy.sh
+++ scripts/deploy-haproxy.sh
@@ -3,7 +3,7 @@
 echo "Deploying HAProxy....!!!"
 echo "Installing the repository"
 
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install ansible -y
 
