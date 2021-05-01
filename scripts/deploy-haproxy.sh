5b10ccdfb78c80197696fbfe6f016b0e6873890d
ArunaLakmal <arunarandunu@gmail.com>
2021-05-01 09:07:18 +0530

    scripts

---
+++ scripts/deploy-haproxy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Deploying HAProxy....!!!"
+echo "Installing the repository"
+
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install ansible -y
+
+echo "Setting up unit files"
+
+cp ../units/* /etc/systemd/system/
+
+systemctl enable lucidwrapper.service
+systemctl enable lucidwrapper.timer
+systemctl daemon-reload
+systemctl start lucidwrapper.service
+systemctl start lucidwrapper.timer
