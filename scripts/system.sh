ff2a94ea2d29d033f10055e9e49f4185033bd509
ArunaLakmal <arunarandunu@gmail.com>
2021-05-02 18:13:49 +0530

    systemd edited

--- scripts/system.sh
+++ scripts/system.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+HAPROXY_LOC=/run/haproxy/
+
 apt install iproute2 -y
 wget https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl.py -O /usr/local/bin/systemctl
-mkdir /run/haproxy/
+chmod +x /usr/local/bin/systemctl
+
+if [[ -d "$HAPROXY_LOC" ]]; then
+        echo "haproxy.location exists"
+else
+        mkdir /run/haproxy/
+fi
