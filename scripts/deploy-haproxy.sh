68f974fd5af74c9549b4bd44b6944de7d28eee7c
ArunaLakmal <arunarandunu@gmail.com>
2021-05-02 17:30:29 +0530

    remove sudo

--- scripts/deploy-haproxy.sh
+++ scripts/deploy-haproxy.sh
@@ -16,9 +16,9 @@ EOF
 echo "Deploying HAProxy....!!!"
 echo "Installing the repository"
 
-sudo apt-add-repository ppa:ansible/ansible -y
-sudo apt-get update
-sudo apt-get install ansible -y
+apt-add-repository ppa:ansible/ansible -y
+apt-get update
+apt-get install ansible -y
 
 echo "Setting up unit files"
 
